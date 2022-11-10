79b6f18cc59c09d5fb8dcbd39106f9df6e996b9c
Jonathan S <jshambaugh96@gmail.com>
2022-11-09 19:42:46 -0600

    version update

--- ImportEvents.py
+++ ImportEvents.py
@@ -2,7 +2,7 @@
 #AUTHOR:Jonathan Shambaugh
 #PURPOSE: To extract the comments given in a Toyopuc project and write them to the corresponding address in the template for easy event importing.
 #NOTES: See the github repository for more info. https://github.com/jdsdev96/EDC-ImportEventsTool
-#VERSION: v2.7.8
+#VERSION: v1.0.0
 #START DATE: 17 Oct 22
 
 from sys import executable, version, modules
